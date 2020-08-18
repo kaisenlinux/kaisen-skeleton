c11fe0cc8a6a706be8611ef4a933775c38ca062f
Kevin Chevreuil - Kaisen <kaisen@kaisen-linux.org>
2020-08-19 01:39:18 +0200

    Add firefox profile, update conky, add conkycolors, conkyconvert and more

--- Makefile
+++ Makefile
@@ -1,8 +1,23 @@
-all:
+PREFIX=/usr
+VPATH=src/
+CFLAGS:=$(CFLAGS) -fcommon -DDESTDIR=\"$(DESTDIR)$(PREFIX)\" -Wall -std=c99
+CWD:=$(shell pwd)
 
-clean:
+all: conky-colors
+conky-colors: conky-colors.c conkycover.c conkyplayer.c \
+	conkyrc_cairo.c conkyrc_ring.c conkyrc_board.c conkyrc_default.c conkyrc_slim.c conkyrc_sls.c \
+	coverposition.c finddir.c help.c options.c photoposition.c themes.c translations.c variables.c \
+	confinstall.c utils.c initialize.c
 
-install:
+install: conky-colors conkyrc
+	mkdir -p $(DESTDIR)/usr
+	install -Dm755 conky-colors $(DESTDIR)$(PREFIX)/bin/conky-colors
+	find conkycolors/bin -type f -exec install -Dm755 {} $(DESTDIR)$(PREFIX)/share/{} \;
+	find conkycolors/scripts -type f -exec install -Dm755 {} $(DESTDIR)$(PREFIX)/share/{} \;
+	find conkycolors/icons -type f -exec install -Dm644 {} $(DESTDIR)$(PREFIX)/share/{} \;
+	find conkycolors/templates -type f -exec install -Dm644 {} $(DESTDIR)$(PREFIX)/share/{} \;
+	find fonts/conkycolors -type f -exec install -Dm644 {} $(DESTDIR)$(PREFIX)/share/{} \;
+	ln -fs $(DESTDIR)$(PREFIX)/share/conkycolors/bin/conkyTask $(DESTDIR)$(PREFIX)/bin/ct
 	mkdir -p $(DESTDIR)/usr/share/kaisen-skeleton
 	cp -f skel/.zshrc $(DESTDIR)/usr/share/kaisen-skeleton
 	cp -f skel/.face $(DESTDIR)/usr/share/kaisen-skeleton
@@ -12,3 +27,17 @@ install:
 	cp -rf skel/.local $(DESTDIR)/usr/share/kaisen-skeleton
 	cp -rf skel/Desktop $(DESTDIR)/usr/share/kaisen-skeleton
 	cp -f skel/.conkyrc $(DESTDIR)/usr/share/kaisen-skeleton
+
+conkyrc: conky-colors
+
+clean:
+	rm -f conky-colors
+
+uninstall:
+	rm -rf $(DESTDIR)$(PREFIX)/share/conkycolors
+	rm -fr $(DESTDIR)$(PREFIX)/share/fonts/conkycolors
+	rm $(DESTDIR)$(PREFIX)/bin/conky-colors
+	rm $(DESTDIR)$(PREFIX)/bin/ct
+
+.PHONY: all clean install uninstall
+
---
+++ firefox/00kaisen.js
@@ -0,0 +1,38 @@
+//Firefox settings for Kaisen Linux
+
+lockPref("app.update.enabled", false);
+lockPref("app.update.auto", false);
+lockPref("app.update.mode", 0);
+lockPref("app.update.service.enabled", false);
+lockPref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+lockPref("browser.newtabpage.activity-stream.telemetry", false);
+lockPref("browser.newtabpage.activity-stream.telemetry.structuredIngestion", false);
+lockPref("browser.newtabpage.activity-stream.telemetry.structuredIngestion.endpoint", "");
+lockPref("browser.ping-centre.telemetry", false);
+lockPref("browser.urlbar.eventTelemetry.enabled", false);
+lockPref("security.app_menu.recordEventTelemetry", false);
+lockPref("security.identitypopup.recordEventTelemetry", false);
+lockPref("security.protectionspopup.recordEventTelemetry", false);
+lockPref("toolkit.telemetry.archive.enabled", false);
+lockPref("toolkit.telemetry.bhrPing.enabled", false);
+lockPref("toolkit.telemetry.ecosystemtelemetry.enabled", false);
+lockPref("toolkit.telemetry.firstShutdownPing.enabled", false);
+lockPref("toolkit.telemetry.newProfilePing.enabled", false);
+lockPref("toolkit.telemetry.server", "");
+lockPref("toolkit.telemetry.shutdownPingSender.enabled", false);
+lockPref("toolkit.telemetry.unified", false);
+lockPref("toolkit.telemetry.updatePing.enabled", false);
+lockPref("browser.shell.checkDefaultBrowser", false);
+pref("browser.startup.firstrunSkipsHomepage", true);
+pref("startup.homepage_welcome_url", "https://kaisen-linux.org");
+pref("browser.startup.homepage", "https://www.google.com");
+pref("browser.newtabpage.activity-stream.default.sites", "https://kaisen-linux.org,https://blog.kaisen-linux.org,https://docs.kaisen-linux.org");
+pref("browser.newtabpage.pinned", "[{\"url\":\"https://kaisen-linux.org\",\"label\"Kaisen Linux\",\"searchTopSite\":true}]");
+pref("browser.search.hiddenOneOffs", "Bing,Amazon.com,eBay,Twitter");
+pref("browser.urlbar.placeholderName", "Google");
+pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned", "google");
+pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.searchEngines", "google");
+pref("browser.search.defaultenginename", "Google");
+pref("privacy.trackingprotection.socialtracking.enabled", true);
+pref("privacy.trackingprotection.origin_telemetry.enabled", true);
+pref("privacy.trackingprotection.enabled", true);
