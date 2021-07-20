af68d18b60289ff5cc0a27e166f53589c105ba6d
Kevin Chevreuil - Kaisen <kaisen@kaisen-linux.org>
2021-07-20 18:51:40 +0200

    Update links

--- firefox/00kaisen.js
+++ firefox/00kaisen.js
@@ -26,7 +26,7 @@ lockPref("browser.shell.checkDefaultBrowser", false);
 pref("browser.startup.firstrunSkipsHomepage", true);
 pref("startup.homepage_welcome_url", "https://kaisenlinux.org");
 pref("browser.startup.homepage", "https://duckduckgo.com/");
-pref("browser.newtabpage.activity-stream.default.sites", "https://kaisenlinux.org,https://kaisenlinux.org/blog/,https://kaisenlinux.org/documentation/,https://kaisenlinux.org/manpages/,https://forum.kaisenlinux.org,https://iso.kaisenlinux.org,https://gitlab.com/kaisenlinux,https://github.com/kaisenlinux,https://distrowatch.com/table.php?distribution=kaisen");
+pref("browser.newtabpage.activity-stream.default.sites", "https://kaisenlinux.org,https://kaisenlinux.org/blog/,https://kaisenlinux.org/documentation/,https://kaisenlinux.org/manpages/,https://forum.kaisenlinux.org,https://iso.kaisenlinux.org,https://gitlab.com/kaisenlinux,https://github.com/kaisenlinux,https://distrowatch.com/kaisen");
 pref("browser.newtabpage.pinned", "[{\"url\":\"https://kaisenlinux.org\",\"label\"Kaisen Linux\",\"searchTopSite\":true}]");
 pref("browser.search.hiddenOneOffs", "Bing,Amazon.com,eBay,Twitter");
 pref("browser.urlbar.placeholderName", "DuckDuckGo");
